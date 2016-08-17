29ac6c62b0422c77c3d62896582e12763d17d3d4
TomStuart92 <Thomascstuart@gmail.com>
2016-08-17 16:21:47 +0100

    Add Guard Clause to Release Functionality

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,13 +2,20 @@ require_relative 'bike'
 
 class DockingStation
   attr_reader :bikes
-def initialize
-  @bikes = []
-end
-  def release_bike
-    Bike.new
+
+  def initialize
+    @bikes = []
   end
+
+  def release_bike
+    if @bikes.empty?
+      raise 'There are no bikes available'
+    else
+      @bikes.pop
+    end
+  end
+
   def dock(bike_object)
-@bikes << bike_object
+    @bikes << bike_object
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,21 @@
 require 'docking_station'
 
 describe DockingStation do
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
-  end
+
   it 'docks bikes' do
-    bike = subject.release_bike
+    bike = Bike.new
     expect(subject.dock(bike)).to eq([bike])
   end
+
+  it 'releases working bikes' do
+    bike = Bike.new
+    subject.dock(bike)
+    expect(subject.release_bike).to eq(bike)
+    expect(bike).to be_working
   end
+
+  it 'should raise error if no bikes avaliable' do
+    expect{subject.release_bike}.to raise_error(RuntimeError)
+  end
+
+end
