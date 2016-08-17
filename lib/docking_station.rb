c40affc29aeef9adfe254c48efcbfff8bb467855
matt vickers <matt.vickers@sky.com>
2016-08-17 15:46:46 +0100

    added bike docking functionality via instance variable

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,10 +1,14 @@
 require_relative 'bike'
 
 class DockingStation
+  attr_reader :bikes
+def initialize
+  @bikes = []
+end
   def release_bike
     Bike.new
   end
-  def dock(x)
-
+  def dock(bike_object)
+@bikes << bike_object
   end
 end
